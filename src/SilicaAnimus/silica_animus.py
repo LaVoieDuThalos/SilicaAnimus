7d9c326ccf38c3655e1a3ec19e0e2a3f16bec017
Patator <lmpatator@gmail.com>
2024-11-08 17:02:07 +0100

    Runned ruff ...

--- src/SilicaAnimus/silica_animus.py
+++ src/SilicaAnimus/silica_animus.py
@@ -5,6 +5,7 @@ from os import getenv
 from discord_client import DiscordClient
 from helloasso_client import HelloAssoClient
 
+
 class SilicaAnimus:
     def __init__(self):
         """_summary_
@@ -14,11 +15,12 @@ class SilicaAnimus:
         """
         self.logger = logging.getLogger(__name__)
         self.discord_client = DiscordClient(getenv("DISCORD_TOKEN"))
-        self.helloasso_client = HelloAssoClient(getenv("HELLOASSO_CLIENT_ID"), getenv("HELLOASSO_CLIENT_SECRET"))
+        self.helloasso_client = HelloAssoClient(
+            getenv("HELLOASSO_CLIENT_ID"), getenv("HELLOASSO_CLIENT_SECRET")
+        )
 
     async def run(self) -> bool:
-        """Run the discord client
-        """
+        """Run the discord client"""
         self.logger.info("Running...")
 
         async with asyncio.TaskGroup() as tg:
