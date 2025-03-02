ba8e73d46fc5eaf66cca600ba4722efa79dbb25c
Endyya <endya@protonmail.com>
2025-03-02 20:37:51 +0100

    cleaned some WIP and outdated functions

--- src/SilicaAnimus/discord_client.py
+++ src/SilicaAnimus/discord_client.py
@@ -161,9 +161,6 @@ class MemberProcessModal(discord.ui.Modal,
         self.interaction = interaction
         await interaction.response.defer()
         
-        
-
-
 
 class CheckModal(discord.ui.Modal, title = 'Informations'):
     """
@@ -195,61 +192,6 @@ class CheckModal(discord.ui.Modal, title = 'Informations'):
         await interaction.response.send_message(embed = embed, 
                                                 ephemeral = True)        
 
-# class MyView(discord.ui.View):
-#     """
-#     WIP
-#     """
-#     @discord.ui.button(label = 'Je cherche un adversaire',
-#                        style = discord.ButtonStyle.primary)
-#     async def button_search(self, interaction, button):
-        
-#         embed = interaction.message.embeds[0]
-
-#         for i, field in enumerate(embed.fields):
-#             if field.name == 'Joueurs en attente':
-#                 my_field = field
-#                 field_ID = i
-
-#         new_user = interaction.user.mention
-#         content = my_field.value
-#         if new_user not in my_field.value:
-#             content += f'\n{new_user}'
-
-#         embed.set_field_at(field_ID, name = 'Joueurs en attente',
-#                            value = content,
-#                            inline = False)
-#         await interaction.response.edit_message(embed = embed)
-                
-#     # @discord.ui.button(label = 'Rejoindre une partie',
-#     #                    style = discord.ButtonStyle.success)
-#     # async def button_join(self, interaction, button):
-#     #     content = interaction.message.content
-#     #     print(interaction.message.embeds[0])
-#     #     edit = content + f'\n{interaction.user.mention} a rejoint une partie'
-#     #     await interaction.message.edit(content = edit)
-
-                
-#     @discord.ui.button(label = 'Se retirer',
-#                        style = discord.ButtonStyle.danger)
-#     async def button_exit(self, interaction, button):
-#         embed = interaction.message.embeds[0]
-#         for i, field in enumerate(embed.fields):
-#             if field.name == "Joueurs en attente":
-#                 my_field = field
-#                 field_ID = i
-
-#         user = interaction.user.mention
-#         content = my_field.value
-#         list_val = my_field.value.split('\n')
-#         for val in list_val:
-#             if user in val:
-#                 list_val.remove(val)
-
-#         embed.set_field_at(field_ID, name = 'Joueurs en attente',
-#                            value = '\n'.join(list_val),
-#                            inline = False)
-
-#         await interaction.response.edit_message(embed = embed)
 
 class UpdateMemberButtons(discord.ui.View):
     def __init__(self, logger, embed, *args, **kwargs):
@@ -473,8 +415,6 @@ async def info(interaction: discord.Interaction,
     await interaction.response.send_message(embed = embed,
                                             ephemeral = True)
 
-""""""
-
             
 @app_commands.command(
     description = """
@@ -585,9 +525,6 @@ async def update_member_list(interaction: discord.Interaction):
         value = to_unmem_str_short,
         inline = False)
 
-
-
-
                     
     buttons = UpdateMemberButtons(logger = client.logger, embed = embed)
 
@@ -672,81 +609,6 @@ class DiscordClient:
         self.start_future = None
         self.run = True
 
-                
-                
-        # @self.tree.command(guild = self.thalos_guild)
-        # async def make_table(interaction: discord.Interaction):
-        #     embed = MessageTemplate(
-        #         title = 'Organisation du 14/12/24')
-        #     embed.add_field(name = 'Parties prévues (1v1)',
-        #                     value = '',
-        #                     inline = False)
-        #     embed.add_field(name = 'Joueurs en attente',
-        #                     value = '',
-        #                     inline = False)
-        #     await interaction.response.send_message(embed = embed,
-        #                                             view = MyView())
-        
-            
-    #     @self.client.event
-    #     async def on_message(message) -> None:
-    #         self.logger.debug(
-    #             f"message {message.content} received from {message.author}"
-    #         )
-
-    #         if message.author == self.client.user:
-    #             return
-
-    #         if message.channel.type == discord.ChannelType.private:
-    #             await self.process_dm(message)
-
-    # async def process_dm(self, message: discord.Message) -> None:
-    #     """Process the DMs received by the bot and takes associated actions
-
-    #     Args:
-    #         message (discord.Message): _description_
-    #     """
-    #     await self.populate_guild_data()
-
-    #     member = self.thalos_guild.get_member(message.author.id)
-    #     if member is None:
-    #         self.logger.info(
-    #             f"{message.author.name} dm'd the bot but is not in the server"
-    #         )
-    #         await message.channel.send(
-    #             "Ce bot n'a d'interêt que si vous êtes membre du serveur du thalos !"
-    #         )
-    #         return
-
-    #     if self.thalos_role in member.roles:
-    #         self.logger.info(
-    #             f"{message.author.name} dm'd the bot but already has the role"
-    #         )
-    #         await message.channel.send(
-    #             f"Bonjour {message.author.name}. Vous êtes déjà membre !"
-    #         )
-    #         return
-
-    #     if message.content.startswith("thalosien"):
-    #         content = message.content.split(" ")
-    #         if len(content) < 3:
-    #             await message.channel.send(
-    #                 f'Bonjour {message.author.name}. Pour avoir le rôle membre, tapez "thalosien Prénom Nom ". Le bot va vérifier votre adhésion.'
-    #             )
-    #             return
-
-    #         first_name, last_name = content[1], content[2]
-    #         self.logger.info(f"{message.author.name} tried to get the role")
-    #         await self.process_membership(
-    #             member=member, first_name=first_name, last_name=last_name
-    #         )
-    #         return
-
-    #     self.logger.info(f"{message.author.name} dm'd the bot with a random message")
-    #     await message.channel.send(
-    #         f'Bonjour {message.author.name}. Pour avoir le rôle membre, tapez "thalosien Prénom Nom ". Le bot va vérifier votre adhésion.'
-    #     )
-    #     return
 
     async def start(self) -> bool:
         """Starts the client
@@ -776,119 +638,3 @@ class DiscordClient:
 
         return True
 
-    # async def populate_guild_data(self) -> None:
-    #     """Fills the class attributes with the necessary guild and membership role"""
-    #     if self.thalos_guild is None:
-    #         self.thalos_guild = self.client.get_guild(int(getenv("THALOS_GUILD_ID")))
-    #         if self.thalos_guild is not None:
-    #             self.thalos_role = self.thalos_guild.get_role(
-    #                 int(getenv("MEMBER_ROLE_ID"))
-    #             )
-    #         else:
-    #             self.logger.warning("Could not get the member role")
-    #             return
-
-    # async def get_member(self, user_id: int) -> discord.Member:
-    #     """Get the discord member with user_id
-
-    #     Args:
-    #         user_id (int): The user id
-
-    #     Returns:
-    #         discord.Member: the discord member
-    #     """
-    #     await self.populate_guild_data()
-    #     member = self.thalos_guild.get_member(user_id)
-    #     if member is None:
-    #         self.logger.warning(f"User {user_id} could not be found")
-
-    #     return member
-
-    # async def process_membership(
-    #     self, member: discord.Member, first_name: str, last_name: str
-    # ) -> None:
-    #     async with asyncio.TaskGroup() as tg:
-    #         ha_task = tg.create_task(
-    #             self.helloasso_client.get_membership(
-    #                 first_name=first_name,
-    #                 last_name=last_name,
-    #             )
-    #         )
-
-    #         real_name_gsheet_task = tg.create_task(
-    #             self.gsheet_client.get_member_by_name(
-    #                 first_name=first_name, last_name=last_name
-    #             )
-    #         )
-
-    #         discord_name_gsheet_task = tg.create_task(
-    #             self.gsheet_client.get_member_by_discord_name(discord_name=member.name)
-    #         )
-
-    #     is_member = ha_task.result()
-    #     real_name_member_info: MemberInfo = real_name_gsheet_task.result()
-    #     discord_name_member_info: MemberInfo = discord_name_gsheet_task.result()
-
-    #     if not is_member:
-    #         self.logger.info(
-    #             f"{first_name} {last_name} got denied the role, not a HelloAsso member"
-    #         )
-    #         await member.send(
-    #             "Tu n'es apparemment pas membre. Vérifie que tu as envoyé les mêmes noms et prénoms que lors de ton inscription sur HelloAsso !"
-    #         )
-    #         return
-
-    #     if discord_name_member_info.in_spreadsheet and (
-    #         discord_name_member_info.first_name.lower() != first_name.lower()
-    #         or discord_name_member_info.last_name.lower() != last_name.lower()
-    #     ):
-    #         self.logger.info(
-    #             f"{member.name} got denied the role with {first_name} {last_name}, "
-    #             "this discord name is already associated with {discord_name_member_info.first_name}, {discord_name_member_info.last_name}"
-    #         )
-    #         await member.send(
-    #             "Ce compte est associé à un autre nom. Contactez un admin."
-    #         )
-    #         return
-
-    #     if (
-    #         real_name_member_info.in_spreadsheet
-    #         and real_name_member_info.discord_nickname.lower() != member.name
-    #     ):
-    #         self.logger.info(
-    #             f"{member.name} got denied the role with {first_name} {last_name}, "
-    #             "this discord name is already associated with {discord_name_member_info.discord_nickname}"
-    #         )
-    #         await member.send(
-    #             "Ce nom est associé à un autre pseudo discord. Contactez un admin."
-    #         )
-    #         return
-
-    #     await self.set_membership(member, first_name, last_name)
-
-    # async def set_membership(
-    #     self, member: discord.Member, first_name: str, last_name: str
-    # ) -> None:
-    #     """Set the membership on discord and updates the spreadsheet
-
-    #     Args:
-    #         member (discord.Member): Discord guild member information
-    #         first_name (str): First name of the member
-    #         last_name (str): Last name of the member
-    #     """
-    #     await member.add_roles(self.thalos_role)
-    #     await member.send("Tu es maintenant membre sur le serveur !")
-    #     self.logger.info(f"{member.name} now has the thalos member role")
-
-    #     member_info = MemberInfo(
-    #         first_name=first_name,
-    #         last_name=last_name,
-    #         discord_nickname=member.name,
-    #         server_nickname=member.display_name,
-    #     )
-
-    #     ret = await self.gsheet_client.add_member(member_info)
-    #     if not ret:
-    #         self.logger.error(f"Could not add {member_info} to spreadsheet")
-
-    #     self.logger.info(f"{member_info} added to spreadsheet")
